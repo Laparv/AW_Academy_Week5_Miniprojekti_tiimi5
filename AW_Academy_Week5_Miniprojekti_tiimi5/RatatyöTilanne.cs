72508a58584bf920fcde0dd04ec4c3cdc88c2dd4
Miikka Saarinen <miikka.saarinen@awacademy.training>
2022-07-06 09:16:50 +0300

    Got api working

---
+++ AW_Academy_Week5_Miniprojekti_tiimi5/RatatyöTilanne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace AW_Academy_Week5_Miniprojekti_tiimi5
+{
+    class RatatyöTilanne
+    {
+        public static async Task RataTyö()
+        {
+            // To do
+        }
+    }
+}
