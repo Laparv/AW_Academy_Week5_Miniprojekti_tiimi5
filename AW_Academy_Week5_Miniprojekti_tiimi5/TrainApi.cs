6ffcd9bebdf63052f6a1106ff0bf3b36a3005996
Laparv <lastappar@gmail.com>
2022-07-06 13:21:42 +0300

    created method to find next trains from station to station

--- AW_Academy_Week5_Miniprojekti_tiimi5/TrainApi.cs
+++ AW_Academy_Week5_Miniprojekti_tiimi5/TrainApi.cs
@@ -18,7 +18,16 @@ namespace AW_Academy_Week5_Miniprojekti_tiimi5
             var response = await ApiHelper.RunAsync<Station[]>(url, urlParams);
             return response;
         }
+        
+        //checks next 5 trains
+        public static async Task<LiveTrains[]> CheckSoonTrains(string departure_station, string arrival_station, string startDate)
+        {
+            string urlParams = $"live-trains/station/{departure_station}/{arrival_station}?include_nonstopping=false&limit=5&startDate={startDate}";
 
+            var response = await ApiHelper.RunAsync<LiveTrains[]>(url, urlParams);
+            return response;
+        }
 
     }
+
 }
