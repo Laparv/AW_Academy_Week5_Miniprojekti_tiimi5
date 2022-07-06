fc4b83916926b3f2b65a89664b06c2a4bc0cd5c7
juttavirta <jutta563@gmail.com>
2022-07-06 10:57:55 +0300

    created method GetLiveTrains and updated method TrainsArrivedLate

--- AW_Academy_Week5_Miniprojekti_tiimi5/LateTrains.cs
+++ AW_Academy_Week5_Miniprojekti_tiimi5/LateTrains.cs
@@ -6,12 +6,12 @@ namespace AW_Academy_Week5_Miniprojekti_tiimi5
 {
     public class LateTrains
     {
-        public static int TrainsArrivedLate(List<LiveTrains> liveList)
+        public static int TrainsArrivedLate (LiveTrains[] liveArray)
         {
-            var test = liveList.Where(train => train.runningCurrently == false && train.cancelled == false);
+            var test = liveArray.Where(train => train.runningCurrently == false && train.cancelled == false);
             var test2 = test.Where(train => train.trainCategory == "Long-distance" || train.trainCategory == "Commuter");
             int lateAmount = test2.Where(juna => juna.timeTableRows.Last().differenceInMinutes > 0).Count();
-            int allArrivedTrains = liveList.Where(train => train.runningCurrently == false).Count();
+            int allArrivedTrains = liveArray.Where(train => train.runningCurrently == false).Count();
             int percentageLate = lateAmount / (allArrivedTrains / 100);
             return percentageLate;
         }
