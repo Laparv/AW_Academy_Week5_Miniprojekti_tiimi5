a821e1beada8c58aba6b93f4ed4b04f8c0fc5b66
juttavirta <jutta563@gmail.com>
2022-07-06 10:43:06 +0300

    added filters to code

--- AW_Academy_Week5_Miniprojekti_tiimi5/LateTrains.cs
+++ AW_Academy_Week5_Miniprojekti_tiimi5/LateTrains.cs
@@ -8,8 +8,9 @@ namespace AW_Academy_Week5_Miniprojekti_tiimi5
     {
         public static int TrainsArrivedLate(List<LiveTrains> liveList)
         {
-            var test = liveList.Where(train => train.runningCurrently == false);
-            int lateAmount = test.Where(juna => juna.timeTableRows.Last().differenceInMinutes > 0).Count();
+            var test = liveList.Where(train => train.runningCurrently == false && train.cancelled == false);
+            var test2 = test.Where(train => train.trainCategory == "Long-distance" || train.trainCategory == "Commuter");
+            int lateAmount = test2.Where(juna => juna.timeTableRows.Last().differenceInMinutes > 0).Count();
             int allArrivedTrains = liveList.Where(train => train.runningCurrently == false).Count();
             int percentageLate = lateAmount / (allArrivedTrains / 100);
             return percentageLate;
